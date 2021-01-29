77ff78112133f67f0e93a41bd65cd0cf4ee94c12
Julia Kendo <ya1814@yandex.ru>
2021-01-29 20:31:07 +0300

    replace all sorted calls with order_by on main page

--- blog/views.py
+++ blog/views.py
@@ -2,9 +2,6 @@ from django.db.models import Count
 from django.shortcuts import render
 from blog.models import Comment, Post, Tag
 
-def get_related_posts_count(tag):
-    return tag.posts.count()
-
 
 def serialize_post(post):
     return {
@@ -36,9 +33,9 @@ def index(request):
     fresh_posts = Post.objects.order_by('published_at')
     most_fresh_posts = list(fresh_posts)[-5:]
 
-    tags = Tag.objects.all()
-    popular_tags = sorted(tags, key=get_related_posts_count)
-    most_popular_tags = popular_tags[-5:]
+    most_popular_tags = Tag.objects.annotate(
+        total_posts=Count('posts', distinct=True)
+    ).order_by('-total_posts')[:5]
 
     context = {
         'most_popular_posts': [serialize_post(post) for post in most_popular_posts],
@@ -75,11 +72,13 @@ def post_detail(request, slug):
         "tags": [serialize_tag(tag) for tag in related_tags],
     }
 
-    all_tags = Tag.objects.all()
-    popular_tags = sorted(all_tags, key=get_related_posts_count)
-    most_popular_tags = popular_tags[-5:]
+    most_popular_tags = Tag.objects.annotate(
+        total_posts=Count('posts', distinct=True)
+    ).order_by('-total_posts')[:5]
 
-    most_popular_posts = []  # TODO. Как это посчитать?
+    most_popular_posts = Post.objects.annotate(
+        total_likes=Count('likes', distinct=True)
+    ).order_by('-total_likes')[:5]
 
     context = {
         'post': serialized_post,
@@ -92,11 +91,13 @@ def post_detail(request, slug):
 def tag_filter(request, tag_title):
     tag = Tag.objects.get(title=tag_title)
 
-    all_tags = Tag.objects.all()
-    popular_tags = sorted(all_tags, key=get_related_posts_count)
-    most_popular_tags = popular_tags[-5:]
+    most_popular_tags = Tag.objects.annotate(
+        total_posts=Count('posts', distinct=True)
+    ).order_by('-total_posts')[:5]
 
-    most_popular_posts = []  # TODO. Как это посчитать?
+    most_popular_posts = Post.objects.annotate(
+        total_likes=Count('likes', distinct=True)
+    ).order_by('-total_likes')[:5]
 
     related_posts = tag.posts.all()[:20]
 
