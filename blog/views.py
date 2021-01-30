b7e62371a838fe41bca940c9354f3dd6e5bb9f35
Julia Kendo <ya1814@yandex.ru>
2021-01-30 16:33:19 +0300

    optimize comments count evaluation

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.db import connection
 from django.db.models import Count
 from django.shortcuts import render
 from blog.models import Comment, Post, Tag
@@ -9,6 +10,20 @@ def serialize_post(post):
         "teaser_text": post.text[:200],
         "author": post.author.username,
         "comments_amount": len(Comment.objects.filter(post=post)),
+        "image_url": post.image.url if post.image else None,
+        "published_at": post.published_at,
+        "slug": post.slug,
+        "tags": [serialize_tag(tag) for tag in post.tags.all()],
+        'first_tag_title': post.tags.all()[0].title,
+    }
+
+
+def serialize_post_optimized(post):
+    return {
+        "title": post.title,
+        "teaser_text": post.text[:200],
+        "author": post.author.username,
+        "comments_amount": post.total_comments,
         "image_url": post.image.url if post.image else None,
         "published_at": post.published_at,
         "slug": post.slug,
@@ -29,11 +44,17 @@ def index(request):
     most_popular_posts = Post.objects.annotate(
         total_likes=Count('likes', distinct=True)
     ).order_by('-total_likes')[:5]
+    popular_posts_with_comments = Post.objects.filter(
+        id__in=[post.id for post in most_popular_posts]
+    ).annotate(total_comments=Count('related_comments'))
     popular_posts_with_prefetch = \
-        most_popular_posts.prefetch_related('author')
+        popular_posts_with_comments.prefetch_related('author')
 
-    most_fresh_posts = Post.objects.order_by('-published_at')[:5]
-    fresh_posts_with_prefetch = most_fresh_posts.prefetch_related('author')
+    most_fresh_posts = Post.objects.annotate(
+        total_comments=Count('related_comments', distinct=True)
+    ).order_by('-published_at')[:5]
+    fresh_posts_with_prefetch = \
+        most_fresh_posts.prefetch_related('author')
 
     most_popular_tags = Tag.objects.annotate(
         total_posts=Count('posts', distinct=True)
@@ -41,10 +62,10 @@ def index(request):
 
     context = {
         'most_popular_posts': [
-            serialize_post(post) for post in popular_posts_with_prefetch
+            serialize_post_optimized(post) for post in popular_posts_with_prefetch
         ],
         'page_posts': [
-            serialize_post(post) for post in fresh_posts_with_prefetch
+            serialize_post_optimized(post) for post in fresh_posts_with_prefetch
         ],
         'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
     }
